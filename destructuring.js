8e06856710edd662754176b7af91ec4de088e53e
unknown <Shikder-Vai>
2022-02-26 14:39:29 +0600

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,31 @@
+// get single properti form an array
+
+const phone = { name: "realme ", model: "2pro", price: "3k", camera: "30px" };
+
+// Method 1
+/*
+const name = phone.name;
+const model = phone.model;
+const price = phone.price;
+console.log(name, model, price);
+*/
+//Method 2
+/*
+const { name, model, camera } = phone;
+console.log(name, model, camera);
+*/
+
+// array inside another array
+const companey = {
+  name: "Ns Shop",
+  regiNo: 3434343,
+  itSection: { work: "Web Development", sellary: "30k", handler: "mini ns" },
+  desingning: {
+    softwar: "adobi ilistator",
+    Worker: "5 person",
+    shift: "6 hours",
+  },
+};
+const { Worker, shift, softwar } = companey.desingning;
+const { name } = companey;
+console.log(Worker, shift, name, softwar);

a59388b8de3af9c99cee0968766289dc83b51ee9
unknown <Shikder-Vai>
2022-02-27 13:11:05 +0600

    chainnig added

---
+++ chainnig.js
@@ -0,0 +1,43 @@
+const { apple, orenge } = { cherry: 100, apple: 500, orenge: 400, mango: 300 };
+console.log(apple, orenge);
+
+const [a, b, c] = [34, 33, 55, 32];
+console.log(a, c);
+
+// declare variable based on the name of an object property
+const myObject = { x: 2, y: 50, z: 600, a: 25, b: 68 };
+const { x, b } = myObject;
+// console.log('myObject.p', myObject?.p?.q);
+
+// destructuring array
+const [p, q] = [45, 37, 91, 86];
+// console.log(p, q);
+
+const [best, faltu] = ["momotaj", "poroshi"];
+// console.log(best, faltu);
+const { sky, color, money } = {
+  sky: "blue",
+  soil: "matti",
+  color: "red",
+  money: 500,
+};
+
+//chaining
+
+const company = {
+  name: "GP",
+  ceo: { id: 1, name: "ajmol", food: "fuchka" },
+  web: {
+    work: "website development",
+    employee: 22,
+    framework: "react",
+    tech: {
+      first: "html",
+      second: "css",
+      third: "js",
+    },
+  },
+};
+
+console.log(company?.web?.tech?.third);
+console.log(company?.backend?.tech.third);

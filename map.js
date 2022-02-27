573c3cb4d37746aa1325686a65073331cdaae4dd
unknown <Shikder-Vai>
2022-02-27 14:07:19 +0600

    map added

---
+++ map.js
@@ -0,0 +1,24 @@
+// const numbers = [2, 3, 4, 5, 6, 12];
+
+// function oldS(number) {
+//   return number * 2;
+// }
+
+// const newS = (number) => number * 2;
+
+// const output = [];
+// for (const number of numbers) {
+//   const results = newS(number);
+//   output.push(results);
+// }
+// console.log(output);
+
+// const results = numbers.map(newS);
+// console.log(results);
+
+// const addMd = (name) => "Md" + " " + name;
+const addMd = (name) => `Md ${name}`;
+
+const names = ["Mahmudul", "Shikder", "ns"];
+const output = names.map(addMd);
+console.log(output);
